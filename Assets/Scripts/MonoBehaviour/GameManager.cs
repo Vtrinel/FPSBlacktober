a11b79b46acaeed59599683f79f8bed6be808261
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-10-06 12:22:34 +0200

    Office in factory level

--- Assets/Scripts/MonoBehaviour/GameManager.cs
+++ Assets/Scripts/MonoBehaviour/GameManager.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        Cursor.visible = false;
     }
 
     public void RegisterEntity(Entity p_entity)
